04ac29b4888d1ed9d400b26c2c06cfa10778216a
LucasIriarte9 <lucasiriartematias@gmail.com>
2024-06-17 15:14:59 -0300

    first commit

--- src/components/galleria/galleria.jsx
+++ src/components/galleria/galleria.jsx
@@ -5,28 +5,28 @@ export function Galeria() {
     <div id="galeria" className="h-96 sm:h-screen xl:h-screen 2xl:h-screen">
       <Carousel>
         <img
-          src="../public/biceps.webp"
-          alt="..."
+          src="/../public/biceps.webp"
+          alt="biceps"
           className="bg-top bg-no-repeat bg-cover max-h-full"
         />
         <img
-          src="../public/espalda.webp"
-          alt="..."
+          src="/../public/espalda.webp"
+          alt="espalda"
           className="bg-top bg-no-repeat bg-cover max-h-full"
         />
         <img
-          src="../public/senta.webp"
-          alt="..."
+          src="/../public/senta.webp"
+          alt="senta"
           className="bg-top bg-no-repeat bg-cover max-h-full"
         />
         <img
-          src="../public/traps.webp"
-          alt="..."
+          src="/../public/traps.webp"
+          alt="traps"
           className="bg-top bg-no-repeat bg-cover max-h-full"
         />
         <img
-          src="../public/horiFondo.webp"
-          alt="..."
+          src="/../public/horiFondo.webp"
+          alt="horiFondo"
           className="bg-top bg-no-repeat bg-cover max-h-full"
         />
       </Carousel>
